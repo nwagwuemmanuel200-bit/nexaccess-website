59dd19e2a496c98d49c1c0a5957183d3daac0d55
nwagwuemmanuel200-bit <nwagwuemmanuel200@gmail.com>
2026-01-27 05:45:15 +0800

    Update Google Analytics tracking ID

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-XXXXXXXXXX');
+        gtag('config', 'G-CLTFGX6Z7T');
       `}
     </Script>
   </head>
